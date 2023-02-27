48f69c30b5903d7663f412c193faac88b403e2c8
Stephen Moran <stephenmoran@live.ie>
2023-02-27 07:04:26 +0000

    Controllers added, and async express handler installed

--- backend/controllers/coffeeShopController.js
+++ backend/controllers/coffeeShopController.js
@@ -1,36 +1,42 @@
+const asyncHandler = require('express-async-handler');
+
 // @desc:   Gets all coffee shops
 // @route   GET /api/coffeeshops/
 // @access  PRIVATE
-const getCoffeeShops = (req, res) => {
+const getCoffeeShops = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'Get Coffee Shops' });
-};
+});
 
 // @desc:   Get a single coffee shop
 // @route   GET /api/coffeeshops/:id
 // @access  PRIVATE
-const getCoffeeShop = (req, res) => {
+const getCoffeeShop = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Get Coffee Shop - ${req.params.id} ` });
-};
+});
 
 // @desc:   Create a coffee shop
 // @route   POST /api/coffeeshops
 // @access  PRIVATE
-const createCoffeeShop = (req, res) => {
+const createCoffeeShop = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error('Coffee Shop content can not be empty');
+  }
   res.status(200).json({ message: `Create Coffee Shop ` });
-};
+});
 
 // @desc:   Update a coffee shop
 // @route   PUT /api/coffeeshops/:id
 // @access  PRIVATE
-const updateCoffeeShop = (req, res) => {
+const updateCoffeeShop = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update Coffee Shop - ${req.params.id}` });
-};
+});
 
 // @desc:   Delete a coffee shop
 // @route   DELETE /api/coffeeshops/:id
 // @access  PRIVATE
-const deleteCoffeeShop = (req, res) => {
+const deleteCoffeeShop = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete Coffee Shop - ${req.params.id}` });
-};
+});
 
 module.exports = { getCoffeeShops, getCoffeeShop, createCoffeeShop, updateCoffeeShop, deleteCoffeeShop };
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
 const express = require('express');
 
 // allows a dot env file with environment variables to be used
-const dotenv = require('dotenv').config;
-
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
 const port = process.env.PORT || 4000;
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use('/api/coffeeshops', require('./routes/coffeeShopRoutes'));
+
+// override the default Express error handler
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
